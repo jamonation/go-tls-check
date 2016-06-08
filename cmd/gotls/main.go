18bc5b11a4b48ef4ccf5b6b24ce331afe6f0eeb1
Jamon Camisso <jamonation@gmail.com>
2016-06-08 22:44:25 +0100

    Refactor into a more coherent set of structs and methods

--- cmd/gotls/main.go
+++ cmd/gotls/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
+	//"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/cli"
-	tlschk "github.com/jamonation/go-tls-check"
+	tlschk "github.com/jamonation/gotls"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -18,49 +18,77 @@ func main() {
 	app.Flags = tlschk.AppFlags //flags live in appflags.go
 
 	app.Action = func(c *cli.Context) error {
+		var keyContainer tlschk.KeyContainer
+		var err error
+		/*
+			switch {
+
+			case tlschk.CertFile != "" && tlschk.KeyFile != "":
+				tlschk.CheckKeyPair()
+
+			case tlschk.CertFile != "":
+				_, _, ASN1certs := tlschk.ProcessCerts()
+				if tlschk.Output == "json" {
+					tlschk.PrintJSONCert(ASN1certs)
+				} else {
+					for _, cert := range ASN1certs {
+						tlschk.PrintText(*cert)
+					}
+				}
+
+			case tlschk.KeyFile != "":
+				_, publicKey := tlschk.ProcessKey()
+				keyModulusHash, err := tlschk.ExtractModulus(publicKey)
+				if tlschk.Output == "json" {
+					tlschk.PrintJSONKey(publicKey)
+				} else {
+					fmt.Println("Private key modulus SHA1 hash:", tlschk.HashMaterial(keyModulusHash))
+				}
+
+			case tlschk.Server != "":
+				if tlschk.Host == "" {
+					tlschk.Host = tlschk.Server
+				}
+
+				conn, err := tls.Dial("tcp", tlschk.Host+":"+strconv.Itoa(tlschk.Port), &tls.Config{InsecureSkipVerify: tlschk.InsecureSkipVerify})
+				if err != nil {
+					fmt.Println("Failed to connect: " + err.Error())
+					os.Exit(1)
+				}
+
+				tlschk.CheckCerts(conn)
+				conn.Close()
+			}
+		*/
 
 		switch {
-
-		case tlschk.CertFile != "" && tlschk.KeyFile != "":
-			tlschk.CheckKeyPair()
-
-		case tlschk.CertFile != "":
-			_, _, ASN1certs := tlschk.ProcessCerts()
-			if tlschk.Output == "json" {
-				tlschk.PrintJSONCert(ASN1certs)
-			} else {
-				for _, cert := range ASN1certs {
-					tlschk.PrintText(*cert)
-				}
-			}
-
 		case tlschk.KeyFile != "":
-			_, publicKey := tlschk.ProcessKey()
-			keyModulusHash := tlschk.ExtractModulus(publicKey)
-			if tlschk.Output == "json" {
-				tlschk.PrintJSONKey(publicKey)
-			} else {
-				fmt.Println("Private key modulus SHA1 hash:", tlschk.HashMaterial(keyModulusHash))
-			}
-
-		case tlschk.Server != "":
-			if tlschk.Host == "" {
-				tlschk.Host = tlschk.Server
-			}
-
-			conn, err := tls.Dial("tcp", tlschk.Host+":"+strconv.Itoa(tlschk.Port), &tls.Config{InsecureSkipVerify: tlschk.InsecureSkipVerify})
+			keyContainer, err = tlschk.ProcessKey(keyContainer)
 			if err != nil {
-				fmt.Println("Failed to connect: " + err.Error())
+				fmt.Println(err.Error())
 				os.Exit(1)
 			}
 
-			tlschk.CheckCerts(conn)
-			conn.Close()
+			if tlschk.Output == "json" {
+				keyContainer.PrivateKey.KeyJSON.ModulusSHA1 = keyContainer.PrivateKey.Hash
+				keyContainer.PrivateKey.KeyJSON.Filename = tlschk.KeyFile
+				b, err := json.MarshalIndent(keyContainer.PrivateKey.KeyJSON, "", "  ")
+				if err != nil {
+					fmt.Println(err.Error())
+					os.Exit(1)
+				}
+				fmt.Println(string(b))
+			} else {
+				fmt.Println("Private key modulus SHA1 hash:", keyContainer.PrivateKey.Hash)
+			}
+
+		case tlschk.CertFile != "":
+			keyContainer, err = tlschk.ProcessCerts(keyContainer)
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
 		}
-
-		/* TODOS
-
-		 */
 
 		return nil
 	}
---
+++ tlschk.go
@@ -0,0 +1,162 @@
+package tlschk
+
+import (
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+type CertList struct {
+	PeerCertificates  []*x509.Certificate
+	VerifiedChains    []*x509.Certificate
+	LocalCertificates []*x509.Certificate
+	Bytes             []byte
+}
+
+// KeyJSON is a container for key & associated (or not) certs
+type KeyJSON struct {
+	ModulusSHA1    string `json:"PrivateKeySHA1Modulus"`
+	Filename       string `json:"PrivateKeyFilename"`
+	MatchedCerts   []CertJSON
+	UnmatchedCerts []CertJSON
+}
+
+// CertJSON contains some selected ASN1 fields for json output
+// willing to add more ASN1 fields, or all if requested
+type CertJSON struct {
+	CommonName      string
+	NotBefore       time.Time
+	NotAfter        time.Time
+	SerialNumber    *big.Int
+	SHA1Fingerprint string
+	ModulusSHA1     string
+	Issuer          string
+	IsCA            bool
+	DNSNames        []string
+	EmailAddresses  []string
+	IPAddresses     []net.IP
+	Filename        string
+}
+
+// PrivateKey contains the rsa key, raw byte version, and
+// a JSON representation of the public key & associated cert(s)
+type PrivateKey struct {
+	Key     *rsa.PrivateKey
+	Bytes   []byte
+	Hash    string `json:"public key hash"`
+	KeyJSON KeyJSON
+}
+
+// KeyContainer is the main struct that contains both public and private keys
+type KeyContainer struct {
+	PublicKeys CertList
+	PrivateKey PrivateKey
+}
+
+func readFile(f string) (b []byte, err error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	return data, err
+}
+
+// HashMaterial returns hex encoded SHA1 sums of input strings
+func hashMaterial(material string) string {
+	h := sha1.New()
+	h.Write([]byte(material))
+	shaSum := h.Sum(nil)
+	hash := hex.EncodeToString(shaSum)
+	return hash
+}
+
+// parse a PEM block and return an x509 certificate
+func parseCert(certBlock *pem.Block) *x509.Certificate {
+	decodedCertASN1, err := x509.ParseCertificate(certBlock.Bytes)
+	if err != nil {
+		fmt.Println("Unable to parse public certificate. Error was:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return decodedCertASN1
+}
+
+// decodeKey decodes raw pem bytes, and then returns the rsa private key
+func (k *KeyContainer) decodeKey() (key *rsa.PrivateKey, err error) {
+	decodedKeyBytes, _ := pem.Decode(k.PrivateKey.Bytes)
+	if decodedKeyBytes != nil {
+		key, err = x509.ParsePKCS1PrivateKey(decodedKeyBytes.Bytes)
+		if err != nil {
+			fmt.Println("Unable to parse private key. Error was:")
+			fmt.Println(err.Error())
+			return nil, err
+		}
+	} else {
+		err = errors.New("Could not find a private key")
+		return key, err
+	}
+	return key, nil
+}
+
+func (k *KeyContainer) decodeCerts() (certs []*x509.Certificate) {
+
+	//var cert *pem.Block
+	//var certASN1 *x509.Certificate
+	//var ASN1certs []*x509.Certificate
+	//var publicKeys []interface{}
+	certBlock, rest := pem.Decode(k.PublicKeys.Bytes)
+	if certBlock != nil {
+		certASN1 := parseCert(certBlock)
+		certs = append(certs, certASN1)
+		for len(rest) > 0 {
+			certBlock, rest = pem.Decode(rest)
+			certASN1 = parseCert(certBlock)
+			certs = append(certs, certASN1)
+		}
+	}
+
+	return certs
+
+}
+
+// ProcessKey reads and returns a private key from the filesystem
+func ProcessKey(k KeyContainer) (KeyContainer, error) {
+	var err error
+	k.PrivateKey.Bytes, err = readFile(KeyFile)
+	if err != nil {
+		return k, err
+	}
+	k.PrivateKey.Key, err = k.decodeKey()
+	if err != nil {
+		return k, err
+	}
+	k.PrivateKey.Hash = hashMaterial(k.PrivateKey.Key.N.String())
+	return k, nil
+}
+
+// ProcessCerts reads and returns an array of certificates
+func ProcessCerts(k KeyContainer) (KeyContainer, error) {
+	var err error
+	k.PublicKeys.Bytes, err = readFile(CertFile)
+	if err != nil {
+		return k, err
+	}
+	k.PublicKeys.LocalCertificates = k.decodeCerts()
+	for _, cert := range k.PublicKeys.LocalCertificates {
+		fmt.Println(cert.Subject.CommonName)
+	}
+	return k, nil
+}
